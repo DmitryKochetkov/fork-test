09354b21c37f9e520b989f133aea61d50be81847
Dmitry Kochetkov <dimitri.kochetkov@gmail.com>
2020-03-12 20:34:27 +0300

    Endls2

--- main.cpp
+++ main.cpp
@@ -1,11 +1,12 @@
+#include "MyList.h"
 #include <iostream>
 
 int main() {
     int a = 4, b = 5;
-    std::cout << a + b;
+    std::cout << a + b << std::endl;
 
-    std::cout << a * b;
+    std::cout << a * b << std::endl;
 
-    std::cout << a - b;
+    std::cout << a - b << std::endl;
     return 0;
 }
